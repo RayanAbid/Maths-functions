5796cae66c1561631e10d22dc2fa33d1832a3984
Rayan Abid <rayanabid12345@gmail.com>
2022-10-21 23:51:52 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const modulus = (a, b) => {
+  return a % b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+alert(divide(1, 2));
